c28953b9b25f12f836182801bff8270647a0dd10
Everglow <948719668@qq.com>
2020-09-07 21:32:27 +0800

    Initial commit

---
+++ src/main/java/com/eve/carmera/CarmeraApplication.java
@@ -0,0 +1,13 @@
+package com.eve.carmera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmeraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarmeraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eve/carmera/CarmeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eve.carmera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarmeraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
